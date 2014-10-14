6f4730895206d4a7c0df123b8161d54b8d9df4b7
Wang Lei <wldandan@gmail.com>
2014-10-15 00:56:55 +0800

    clean the username and password date in ruby_wrapper.sh

--- script/ruby_wrapper.sh
+++ script/ruby_wrapper.sh
@@ -6,14 +6,7 @@
 #########################################################################
 set -e
 
-#source /opt/env.sh
-
-export SECRET_KEY_BASE=c538eeec29b5f96b8982b9f621914d9899a769d9f461b077881a07e73027bf7d83d7edc2935b7544f04ef00a04f45f5466500f98247e6a01fed346800a255289x
-export KIDSMEETS_URI=106.186.117.131:27017
-export KIDSMEETS_USERNAME=kidsmeets-admin
-export KIDSMEETS_PASSWORD=k1dsmeets-admin
-export KIDSMEETS_DATABASE=kidsmeets
-
+. /opt/env.sh
 
 export RUBY_HEAP_MIN_SLOTS=600000
 export RUBY_HEAP_SLOTS_INCREMENT=250000
