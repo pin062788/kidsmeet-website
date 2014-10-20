27d3d1cb0eaf3cfd5d573caddf172e9bc197beb6
Wang Lei <wldandan@gmail.com>
2014-10-20 23:32:29 +0800

    add presence validation for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,10 +8,15 @@ class Event
   field :abstract, type: String
   field :content, type: String
   field :main_image_url, type: String
-  field :external_article_url, type: String
   field :apply_end_date, type: Date
   field :apply_number_limitation, type: Integer
 
 
 
+  validates :title, presence: true
+  validates :category, presence: true
+  validates :abstract, presence: true
+  validates :content, presence: true
+  validates :main_image_url, presence: true
+  validates :apply_end_date, presence: true
 end
