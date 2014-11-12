8b33ac24b13dfc70f708a6d13f72543f451009b6
Wang Lei <wldandan@gmail.com>
2014-11-13 02:03:03 +0800

    change the bcc list

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -3,6 +3,6 @@ class AttendanceMailer < ActionMailer::Base
 
   def send_email(attendance)
     @attendance = attendance
-    mail(:to => @attendance.email, :bcc => ["kidsmeetmaster@gmail.com", "wldandan@gmail.com"], :subject => "感谢您的关注[#{attendance.event.title}]")
+    mail(:to => @attendance.email, :bcc => ["kidsmeetmaster@gmail.com", "Wang Lei <wldandan@gmail.com>"], :subject => "感谢您的关注 [#{attendance.event.title}]")
   end
 end
