3b20c89b016cc5a8003b6ecdb927a7eceb6519f7
Wang Lei <wldandan@gmail.com>
2014-11-17 20:18:58 +0800

    add default avartar for attendance

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  AVATARS = %w(cat.png  cow.png  donkey.png  frog.png  mouse.png 
+               panda.png  pig.png  sheep.png  tiger.png)
+
+  def avatar_placeholder_url
+    "/assets/avatar/#{AVATARS[rand(AVATARS.length)]}"
+  end
 end
