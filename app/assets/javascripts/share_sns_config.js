060363aaa3673484b8212b0772f1657c3e5ad07c
zhao-guo <zhao.guo@rea-group.com>
2014-11-30 16:54:47 +0800

    share to sns: UI

---
+++ app/assets/javascripts/share_sns_config.js
@@ -0,0 +1,18 @@
+window._bd_share_config = {
+  common : {
+  bdText : '自定义分享内容',
+  bdDesc : '自定义分享摘要',
+  bdUrl : 'http://localhost:3000/events/11',
+  bdPic : 'http://kidmeets-images.qiniudn.com/11/large_img_20131003_172208.jpg'
+  },
+  share : [{
+    "bdSize" : 16
+    }],
+  slide : [{
+    bdImg : 0,
+    bdPos : "right",
+    bdTop : 100
+    }]
+}
+
+with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?cdnversion='+~(-new Date()/36e5)];
